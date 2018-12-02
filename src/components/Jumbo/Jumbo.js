771d352fbf8391fbbe845427c1499cdd77286d80
Nicole Cloyd <Nicole@nicole-cloyds-macbook-pro.local>
2018-12-02 14:18:15 -0600

    background image

---
+++ src/components/Jumbo/Jumbo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Jumbo = () => (
+
+		<div className="parallax-container">
+		    <div className="section no-pad-bot">
+		      	<div className="container">
+		    
+				</div>
+		    </div>
+		    	<div className="backg"><img src="/background.jpg" alt="sf" />
+		    </div>
+		</div>
+
+);
+
+export default Jumbo;
